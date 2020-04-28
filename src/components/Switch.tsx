124d288362157e0b9e2cfb561f48c0a6fc050446
vanderkilu <vanderkiliku@gmail.com>
2020-04-28 17:53:17 +0000

    remove commit

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 
 const StyledSwitch = styled.button`
   padding: 0.8rem 2rem;
-  /* border: none; */
   border: 1px solid #b8bac3;
   border-radius: 5px;
   background-color: transparent;
